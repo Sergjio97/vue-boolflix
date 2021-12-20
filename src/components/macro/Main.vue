30d8afb1e1cbddd33f134ac176ab70595e7f040b
Sergjio97 <sergiocizmja@gmail.com>
2021-12-20 17:01:51 +0100

    chiamata api attraverso il valore dell'input

--- src/components/macro/Main.vue
+++ src/components/macro/Main.vue
@@ -1,10 +1,37 @@
 <template>
-  <main></main>
+  <main>
+      <nav>
+          <input type="text" placeholder="inserisci il titolo" v-model="inputValue">
+          <button @click="searchFilm">Cerca</button>
+      </nav>
+  </main>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    name: 'Main'
+    name: 'Main',
+    data() {
+        return {
+            inputValue: '',
+        }
+    },
+    methods: {
+        searchFilm() {
+            axios.get('/user', {
+                params: {
+                api_key: 'eebcf7c938b9bfe34e05762eae2bec88',
+                query: this.inputValue
+                }
+            })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        }
+    }
 }
 </script>
 
